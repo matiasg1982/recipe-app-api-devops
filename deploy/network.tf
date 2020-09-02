bc2194c799e3bf010fa97a0d63e1db660bf66921
matias <matiasg1982@gmail.com>
2020-09-02 13:43:50 +0200

    private subnets added

--- deploy/network.tf
+++ deploy/network.tf
@@ -26,7 +26,7 @@ resource "aws_subnet" "public_a" {
   cidr_block              = "10.1.1.0/24"
   map_public_ip_on_launch = true
   vpc_id                  = aws_vpc.main.id
-  availability_zone       = "${data.aws_region.current}a"
+  availability_zone       = "${data.aws_region.current.name}a"
 
   tags = merge(
     local.common_tags,
@@ -51,7 +51,7 @@ resource "aws_route_table_association" "public_a" {
 resource "aws_route" "public_internet_access_a" {
   route_table_id         = aws_route_table.public_a.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id              = aws_internet_gateway.main.id
+  gateway_id             = aws_internet_gateway.main.id
 }
 
 resource "aws_eip" "public_a" {
@@ -73,11 +73,13 @@ resource "aws_nat_gateway" "public_a" {
   )
 }
 
+### Public b ###
+
 resource "aws_subnet" "public_b" {
   cidr_block              = "10.1.2.0/24"
   map_public_ip_on_launch = true
   vpc_id                  = aws_vpc.main.id
-  availability_zone       = "${data.aws_region.current}b"
+  availability_zone       = "${data.aws_region.current.name}b"
 
   tags = merge(
     local.common_tags,
@@ -122,4 +124,74 @@ resource "aws_nat_gateway" "public_b" {
     local.common_tags,
     map("Name", "${local.prefix}-public-b")
   )
+}
+
+####################################################
+# Private subnets - Outbound Internet Access only #
+####################################################
+
+resource "aws_subnet" "private_a" {
+  cidr_block        = "10.1.10.0/24"
+  vpc_id            = aws_vpc.main.id
+  availability_zone = "${data.aws_region.current.name}a"
+
+
+  tags = merge(
+    local.common_tags,
+    map("Name", "${local.prefix}-private-a")
+  )
+}
+
+resource "aws_route_table" "private_a" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    map("Name", "${local.prefix}-private-a")
+  )
+}
+
+### Private b ###
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private_a.id
+}
+
+resource "aws_route" "private_a_internet_out" {
+  route_table_id         = aws_route_table.private_a.id
+  nat_gateway_id        = aws_nat_gateway.public_a.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+
+resource "aws_subnet" "private_b" {
+  cidr_block        = "10.1.11.0/24"
+  vpc_id            = aws_vpc.main.id
+  availability_zone = "${data.aws_region.current.name}b"
+
+
+  tags = merge(
+    local.common_tags,
+    map("Name", "${local.prefix}-private-b")
+  )
+}
+
+resource "aws_route_table" "private_b" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    map("Name", "${local.prefix}-private-b")
+  )
+}
+
+resource "aws_route_table_association" "private_b" {
+  subnet_id      = aws_subnet.private_b.id
+  route_table_id = aws_route_table.private_b.id
+}
+
+resource "aws_route" "private_b_internet_out" {
+  route_table_id         = aws_route_table.private_b.id
+  nat_gateway_id        = aws_nat_gateway.public_b.id
+  destination_cidr_block = "0.0.0.0/0"
 }
