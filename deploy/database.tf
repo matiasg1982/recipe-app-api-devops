484406d1de8d9439ed039dfa89dcbeb8185da7b2
matias <matiasg1982@gmail.com>
2020-09-03 12:12:29 +0200

    add dt tf

--- deploy/database.tf
+++ deploy/database.tf
@@ -26,20 +26,20 @@ resource "aws_security_group" "rds" {
 }
 
 resource "aws_db_instance" "main" {
-  identifier               = "${local.prefix}-db"
-  name                     = "recipe"
-  allocated_storage        = 20
-  storage_type             = "gp2"
-  engine                   = "postgress"
-  engine_version           = "11.4"
-  instance_class           = "db.t2.micro"
-  db_subnet_group_name = aws_db_subnet_group.main.name
-  password                 = var.db_password
-  username                 = var.db_username
-  backup_retention_period  = 0
-  multi_az                 = false
-  skip_final_snapshot      = true
-  vpc_security_group_ids   = [aws_security_group.rds.id]
+  identifier              = "${local.prefix}-db"
+  name                    = "recipe"
+  allocated_storage       = 20
+  storage_type            = "gp2"
+  engine                  = "postgress"
+  engine_version          = "11.4"
+  instance_class          = "db.t2.micro"
+  db_subnet_group_name    = aws_db_subnet_group.main.name
+  password                = var.db_password
+  username                = var.db_username
+  backup_retention_period = 0
+  multi_az                = false
+  skip_final_snapshot     = true
+  vpc_security_group_ids  = [aws_security_group.rds.id]
 
   tags = merge(
     local.common_tags,
