8257372ca10c22fae3014ba2754ef3ba77e489b1
matias <matiasg1982@gmail.com>
2020-09-08 13:13:36 +0200

    added ecs service

--- deploy/ecs.tf
+++ deploy/ecs.tf
@@ -79,6 +79,13 @@ resource "aws_security_group" "ecs_service" {
   vpc_id      = aws_vpc.main.id
 
   egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
     from_port = 5432
     to_port   = 5432
     protocol  = "tcp"
