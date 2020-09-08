ed1cd0f3a7e1f3954aa18eb9f9249fcad71a5e91
matias <matiasg1982@gmail.com>
2020-09-08 12:15:38 +0200

    ecs added

--- deploy/variables.tf
+++ deploy/variables.tf
@@ -21,3 +21,17 @@ variable "db_password" {
 variable "bastion_key_name" {
   default = "recipe-app-api-devops-key-bastion"
 }
+
+variable "ecr_image_api" {
+  default     = "719553580325.dkr.ecr.us-east-1.amazonaws.com/recipe-app-api-devops:lastest"
+  description = "ECR image for api"
+}
+
+variable "ecr_image_proxy" {
+  default     = "719553580325.dkr.ecr.us-east-1.amazonaws.com/recipe-app-api-proxy:lastest"
+  description = "ECR image for proxy"
+}
+
+variable "django_secret_key" {
+  description = "Secret key for Django app"
+}
